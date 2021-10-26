7dbb838d44877ee364734d8b97aa52a58c0b6249
vishweshkadam <75071313+vishweshkadam@users.noreply.github.com>
2021-10-26 16:21:18 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,11 @@
+function setup(){
+    canvas=createCanvas(450,380)
+    video = createCapture(VIDEO)
+    canvas.position(700, 140)
+    video.position(100, 80)
+    
+}
+
+function draw(){
+   
+}
